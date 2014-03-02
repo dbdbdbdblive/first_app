9c7e0cf15a067d7f8542a459cf0aa2a3a7a97b25
db <dbdbdbdblive@gmail.com>
2014-03-01 22:45:06 -0800

    Initial commit of first_app project, setup as per http://ruby.railstutorial.org
    Updated .gitignore as recommended at http://ruby.railstutorial.org

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'c0ef58d565884e296689a159ddbbd6d89d6a452fa0fe797961ef24a82d1113371d5fddc14357633cdf39d2e73c42ceefcde51270bcceb52561955058b469b4a1'
